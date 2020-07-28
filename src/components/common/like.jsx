9b205c3f995990123a63477d77e346c15c7de742
JuniorBaymx <cooladitya953@gmail.com>
2020-07-28 16:36:42 +0530

    added like button functionality

---
+++ src/components/common/like.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+const Like = (props) => {
+    
+	let classes = 'fa fa-heart';
+
+	if (!props.liked) classes += '-o';
+	return (
+		<i
+			onClick={props.onClick}
+			className={classes}
+			aria-hidden="true"
+			style={{ cursor: 'pointer' }}></i>
+	);
+};
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
-
+import Like from './common/like';
 class Movies extends React.Component {
 	state = {
 		movies: getMovies(),
+	};
+
+	handleLike = (movie) => {
+		const movies = [...this.state.movies];
+		const idx = movies.indexOf(movie)
+		movies[idx] = {...movies[idx]}
+
+		movies[idx].liked = !movies[idx].liked;
+
+		this.setState({movies})
 	};
 
 	handleDelete = (movie) => {
@@ -12,8 +22,14 @@ class Movies extends React.Component {
 		this.setState({ movies });
 	};
 	render() {
+		const { length: count } = this.state.movies;
+
+		if (count === 0) {
+			return <p>There is no movies in the database</p>;
+		}
 		return (
-			<div>
+			<React.Fragment>
+				<p>showing {count} movies in the database</p>
 				<table className="table">
 					<thead>
 						<tr>
@@ -32,6 +48,12 @@ class Movies extends React.Component {
 								<td>{movie.numberInStock}</td>
 								<td>{movie.dailyRentalRate}</td>
 								<td>
+									<Like
+										liked={movie.liked}
+										onClick={() => this.handleLike(movie)}
+									/>
+								</td>
+									<td>
 									<button
 										className="btn btn-md btn-danger"
 										onClick={() => this.handleDelete(movie)}>
@@ -42,7 +64,7 @@ class Movies extends React.Component {
 						))}
 					</tbody>
 				</table>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
